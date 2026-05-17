53df953d606857aaef8502f6a7f3f5eb441c5594
AvsaStudio <salome.a.miller@gmail.com>
2026-05-17 07:13:59 -0400

    hardcoded guests array

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
+import { useState } from "react";
+
 export default function App() {
+  const [guests, setGuests] = useState([]);
+  const [selectedGuestId, setSelectedGuestId] = useState(null);
+  const [selectedGuest, setSelectedGuest] = useState(null);
+  const [loading setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
   return <></>;
 }
+
+export async function getGuests() {}
+export async function getGuest(id) {}
